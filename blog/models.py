b51cb004875149ae26f1ee0757db12b7a40b7ac7
Rodolfo Talan <rtalan@gmail.com>
2020-08-25 17:54:37 -0600

    Show all blog posts on the home page.

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-
+# create string representation
     def __str__(self):
         return self.title
 
