c68f38eec8896177deaedbaa62185ed0c1b234b9
Rodolfo Talan <rtalan@gmail.com>
2020-08-26 00:53:27 -0600

    Add feature to edit blog posts from frontend.

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # 127.0.0.1:8000 --> local
     # mydjangosite.com --> online
     path('', views.post_list, name='post_list'),
+
     # 127.0.0.1:8000/post/2 --> local
     # mydjangosite.com/post/2 --> online
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
@@ -12,4 +13,8 @@ urlpatterns = [
     # 127.0.0.1:8000/post/new --> local
     # mydjangosite.com/post/new --> online
     path('post/new/', views.post_new, name='post_new'),
+
+    # 127.0.0.1:8000/post/2/edit --> local
+    # mydjangosite.com/post/2/edit --> online
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
