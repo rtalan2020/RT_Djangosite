05b7c6ad6d6e2ccab95b78a498dbe7e3547c566d
Rodolfo Talan <rtalan@gmail.com>
2020-08-27 22:08:00 -0600

    Add logout functionality.

--- RTsite/urls.py
+++ RTsite/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # 127.0.0.1:8000/accounts/login --> local
+    # mydjangosite.com/accounts/login --> online
+    # path('accounts/login/', auth_views.login, name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    # or this will do path('accounts/login/', auth_views.LoginView.as_view(template_name="registration/login.html"), name='login'),
+
+	# 127.0.0.1:8000/accounts/logout --> local
+    # mydjangosite.com/accounts/logout --> online
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+
     path('', include('blog.urls'))
 ]
