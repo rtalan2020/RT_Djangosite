0aaf35d3802ec405e155fd7441d03c5fd423fe5c
Rodolfo Talan <rtalan@gmail.com>
2020-08-28 03:02:21 -0600

    Add feature to remove comments from blog posts.

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm, CommentForm
+from .models import Post, Comment
 
 # Create your views here.
 def post_list(request):
@@ -60,6 +61,7 @@ def post_publish(request, pk):
     post.publish()
     return redirect('post_detail', pk=pk)
 
+@login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
@@ -73,3 +75,9 @@ def add_comment_to_post(request, pk):
     else:
        form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form, 'title':post.title})
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
